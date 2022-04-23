fc0fa275f1d29c2a52c7c0c72b9651da53047303
Alkondr223322 <oleksii.kondrashin@gmail.com>
2022-04-23 19:42:22 +0300

    fixed file upload

--- multer.js
+++ multer.js
@@ -3,12 +3,11 @@ const multer = require('multer');
 // set storage
 var storage = multer.diskStorage({
     destination : function ( req , file , cb ){
-        cb(null, './uploads')
+        cb(null, 'uploads')
     },
     filename : function (req, file , cb){
         // image.jpg
         var ext = file.originalname.substring(file.originalname.lastIndexOf('.'));
-
         cb(null, file.fieldname + '-' + Date.now() + ext)
     }
 })
